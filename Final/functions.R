50c24f08d8d344a4dfda5fb83ade34f5b6ac576e
Lisa Hu <ljbhu2002@gmail.com>
2022-06-03 16:14:07 +0200

    Finished plots of the paper

--- Final/functions.R
+++ Final/functions.R
@@ -1,12 +1,27 @@
-## Model function
+## ---------------------------
+##
+## Name: functions.R
+##
+## Author: Lisa Hu
+##
+## Purpose: Script contains functions used in the result scripts
+##
+## Email: l.j.b.hu@st.hanze.nl
+##
+## ---------------------------
+
+## ---- basic-model
+#' Basic model
 model <- function(t, y, parms){
-  if(t %% 9 == 0 && t < 80 && t > 0){
+  # Add water every 8 days, until day 80
+  if(t %% 8 == 0 && t < 80 && t > 0){
     with(as.list(c(parms, y)), {
-      dW <- I
-      dC <- 0
+      dW <- I # I is the water irrigation
+      dC <- 0 # There is no growth on those days
       return( list( c(dW, dC) ) )
     })
   }
+  # Else the model runs with the differentials
   else{
     with(as.list(c(parms, y)),{
       dW <- (-B * q * W) - (r * C * (1 - ( C/N ) ) * W)
@@ -16,8 +31,33 @@ model <- function(t, y, parms){
   }
 }
 
+## ---- delayWater-model
+#' Model for delayed water irrigation
+delay.model <- function(t, y, parms){
+  # Add water every 16 days, until day 80
+    if(t %% 16 == 0 && t < 80 && t > 0){
+    with(as.list(c(parms, y)), {
+      dW <- I # I is the water irrigation
+      dC <- 0 # There is no growth on those days
+      return( list( c(dW, dC) ) )
+    })
+  }
+  # Else the model runs with the differentials
+  else{
+    with(as.list(c(parms, y)),{
+      dW <- (-B * q * W) - (r * C * (1 - ( C/N ) ) * W)
+      dC <- (r * C * (1 - ( C/N ) ) * W) + ( (g*q*C*W)/(C+1)*(W+1) ) - o * C
+      return( list( c(dW, dC) ) )
+    })
+  }
+}
+
+
 ## Function to create plots
 create.plots <- function(plot.values, ref.data, change.data){
+  #' plot.values = The column name of the datas
+  #' ref.data = The reference data
+  #' change.data = The data that contains changed values
   data.names <- names(change.data)
   # Create colours for the different lines (except the reference data)
   colours <- hue_pal()(length(change.data))
@@ -26,7 +66,7 @@ create.plots <- function(plot.values, ref.data, change.data){
   # The plot
   plt <- ggplot(data = ref.data, mapping = aes(x = time, y = !!sym(y.val$name) ) ) +
     # Lines (Reference data stays black)
-    geom_line(aes(color = "Ref")) +
+    geom_line(aes(color = "Reference")) +
     unlist( mapply(function(single.data, data.name)
                         geom_line(data = single.data, aes(color = data.name) ),
                    change.data, data.names ) ) +
@@ -35,15 +75,16 @@ create.plots <- function(plot.values, ref.data, change.data){
     theme(legend.position = "bottom") +
     # Line colours
     scale_colour_manual(values = c("black", colours),
-                        limits = c("Ref", names(change.data) ) ) +
+                        limits = c("Reference", names(change.data) ) ) +
     # Legend correction
     guides(color = guide_legend(title = ""))
+  print(plt)
   return(plt)
 }
 
 ## Function that arranges plots
 arrange.plots <- function(plots, title){
   my.grid <- ggarrange(plotlist = plots, ncol = 2, nrow = length(plots)/2,
-                       common.legend = TRUE, legend = "bottom")
+                       common.legend = FALSE, legend = "bottom")
   print( annotate_figure(my.grid, top = text_grob(title) ) )
 }
--- Final/results1.R
+++ Final/results1.R
@@ -1,23 +1,62 @@
-## Set the values for the simulation
-parameters <- c(
-  q = 0.5, # Accumulated energy constant
-  r = 0.043, # Fruit trees intrinsic growth rate
-  N = 3000, # Fruit carrying capacity
-  I = 1, # Water toevoer
-  B = 0.06, # Evapotranspiration rate
-  g = 0.001, # Photosynthetic contribution rate
-  o = 0.00001 # Mortality rate of fruit trees,
-)
+## ---------------------------
+##
+## Name: results1.R
+##
+## Author: Lisa Hu
+##
+## Purpose: Script creates the first results for the final report
+##
+## Email: l.j.b.hu@st.hanze.nl
+##
+## ---------------------------
 
+## ODE values
+parameters <- c(q = 0.5, r = 0.043, N = 3000, I = 1, B = 0.06, g = 0.001, o = 0.00001)
 state <- c(W = 0.6, C = 1)
 times <- seq(0, 120, by = 1)
 
-## Run the simulation
-results <- as.data.frame(ode(times = times, y = state,
-                 parms = parameters, func = model, method = "euler"))
+data.values <- data.frame(name = c("W", "C"),
+                          ylabel = c("W(t)", "C(t)"),
+                          title = c("Soil water amount", "Growth of fruit tree biomass"))
+rownames(data.values) <- data.values$name
 
-ggplot(results, mapping = aes(x = time)) +
-  geom_line(mapping = aes(y = W), color = "blue") +
-  geom_line(mapping = aes(y = C), color = "red", linetype = "dashed")+
-  geom_vline(xintercept = 10) +
-  labs(x = "Time", y = "W(t), C(t)")
+
+## Run the simulations
+ref.data <- as.data.frame(ode(times = times, y = state,
+                              parms = parameters, func = model, method = "euler"))
+
+q.values <- list("q = 0.1" = 0.1,
+                 "q = 1" = 1)
+
+for(i in seq_along(q.values)){
+  parameters$q <- q.values[[i]]
+  q.values[[i]] <- as.data.frame(ode(times = times, y = state,
+                                     parms = parameters, func = model, method = "euler"))
+}
+
+delay.data <- as.data.frame(ode(times = times, y = state, parms = parameters,
+                                func = delay.model, method = "euler"))
+delay.data <- list("Delayed water irrigation" = delay.data)
+
+
+## plot the data
+plt1 <- ggplot(ref.data, mapping = aes(x = time)) +
+          geom_line(mapping = aes(y = W, color = "Water")) +
+          geom_line(mapping = aes(y = C, color = "Biomass"), linetype = "dashed") +
+          labs(x = "Time", y = "W(t), C(t)") +
+          scale_colour_manual(values = c("blue", "red"),
+                              limits = c("Water", "Biomass")) +
+          guides(color = guide_legend(title = "", override.aes = list(linetype = c(1, 2))))
+
+plt2 <- lapply("C", create.plots, ref.data, q.values)
+plt3 <- lapply(c("W", "C"), create.plots, ref.data, delay.data)
+
+plot.list <- append(list(plt1), c(plt2, plt3))
+
+plot.tags <- c("(a)", "(b)", "(c)", "(d)")
+
+for(i in seq_along(plot.list)){
+  plot.list[[i]] <- plot.list[[i]] + labs(tag = plot.tags[i])
+}
+
+arrange.plots(plots = plot.list, title = "Plots of the paper")
